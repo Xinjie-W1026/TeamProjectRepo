f286a5fd27dd7a4cbbde0de53d7416e18780ab7f
Xinjie-W1026 <202483910024@nuist.edu.cn>
2024-12-08 16:44:54 +0800

    update Shopping Cart Class

--- OnlineShoppingApp/src/ShoppingCart.java
+++ OnlineShoppingApp/src/ShoppingCart.java
@@ -2,30 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    private List<Product> productList;
+    private List<Products> productList;
     public ShoppingCart(){
         productList = new ArrayList<>();
     }
-    public void addProduct(Product product){
-        productList.add(product);
-    }
-    public Product findProduct(String productName){
-        Product foundProduct = null;
-        if(!productList.isEmpty()) {
-            for(Product p :productList){
-                if(p.getProductName().equals(productName)) {
-                    foundProduct = p;
-                    break;
-                }
-            }
+    public void addProduct(Product product,int quantity){
+        if(quantity<=0||product == null) return ;
+        int foundindex = findProduct(product.getProductName());
+        if(foundindex != -1){
+            productList.get(foundindex).setQuantity(quantity + productList.get(foundindex).getQuantity());
         }
-        return foundProduct;
+        else {
+            productList.add(new Products(product,quantity));
+        }
+    }
+
+     private int findProduct(String productName){
+        Products foundProduct = null;
+        if(!productList.isEmpty()) {
+            for(int i = 0;i < productList.size();i++)
+                if(productList.get(i).getItem().getProductName().equals(productName)) {
+                    return i;
+                }
+        }
+        return -1;
     }
     public boolean removeProduct(String productName){
         if(!productList.isEmpty()) {
-            Product foundRemoveProduct = findProduct(productName);
-            if (foundRemoveProduct != null) {
-                productList.remove(foundRemoveProduct);
+            int foundindex = findProduct(productName);
+            if (foundindex != -1) {
+                productList.remove(foundindex);
                 return true;
             }
             return false;
@@ -34,19 +40,45 @@ public class ShoppingCart {
             return false;
     }
     public double totalprice(){
-        double sum=0;
-        for(Product p :productList)
-            sum += p.getPrice();
+        double sum = 0;
+        for(Products p :productList)
+            sum += p.getItem().getPrice()*p.getQuantity();
         return sum;
     }
     public void showProduct(){
         if(!productList.isEmpty()){
             System.out.println("The total price is "+totalprice());
-            for(Product p :productList)
+            for(Products p :productList)
                 System.out.println(p);
         }
         else{
             System.out.println("No product in the Shopping Cart");
+        }
+    }
+    private class Products {
+        private Product item;
+        private int quantity;
+
+        public Products(Product item, int quantity) {
+            this.item = item;
+            setQuantity(quantity);
+        }
+
+        public Product getItem() {
+            return item;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            if(quantity > 0) {
+                this.quantity = quantity;
+            }
+            else{
+                System.out.println("Unable to set quantity, Must be positive");
+            }
         }
     }
 }
